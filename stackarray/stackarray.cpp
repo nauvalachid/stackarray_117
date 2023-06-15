b0eb4d893d381e306672b258658705ca4f79288e
nauvalachid <nauvalachid@gmail.com>
2023-06-15 14:55:58 +0700

    membuat class stackarray & constructor

---
+++ stackarray/stackarray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class StackArray {
+private:
+	string stack_array[5];
+	int top;
+public:
+	//constructor
+	StackArray() {
+		top = -1;
+	}
